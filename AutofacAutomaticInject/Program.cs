8db5efcaa28f52f802c524f53caf0fdd1baab040
Grigorcea Ana <ana.grigorcea@gmail.com>
2018-11-29 17:27:25 +0200

    Add project files.

---
+++ AutofacAutomaticInject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutofacAutomaticInject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
